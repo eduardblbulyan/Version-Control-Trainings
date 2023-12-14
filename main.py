b7d9d2bd6f142013b7e3edc177b810318f863a37
Edward <edw.blb@gmail.com>
2023-12-14 23:17:31 +0400

    Exceptions and error catch

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 def multiply(x, y):
-    return x*y
+    try:
+        return x*y
+    except TypeError:
+        return "Type Error"
+
 
 def summer(x, y):
-    return x+y
+    try:
+        return x+y
+    except TypeError:
+        return "Type Error"
+
+
+def diver(x, y):
+    try:
+        return x/y
+    except ZeroDivisionError:
+        return "Zero division error"
+    except TypeError:
+        return "Type Error!"
